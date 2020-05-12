d980a11868d0e612a8852ed51e1bb2b509f882dc
Danep1 <57663920+Danep1@users.noreply.github.com>
2020-05-12 13:43:26 +0300

    Delete db_test.py

--- db_test.py
+++
@@ -1,9 +0,0 @@
-import pyodbc as odbc
-
-conn = odbc.connect(r'Driver={Microsoft Access Driver (*.mdb, '
-                    r'*.accdb)};DBQ=db\Кулинарная книга2.mdb;')
-cursor = conn.cursor()
-cursor.execute('select cooking from Книга where id_1=1')
-
-for row in cursor.fetchall():
-    print(row)
